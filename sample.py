f7c661a552d1ac259dc5e197e4483dcf2c2088ac
angelica1997 <zhixin.feng@gmail.com>
2016-09-17 01:41:44 -0400

    clustering

--- sample.py
+++ sample.py
@@ -1,20 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
 from googleplaces import GooglePlaces, types, lang
 import json
 import urllib
 
-location = 'London'
-YOUR_API_KEY = 'AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I'
+@app.route("/")
+def hello():
+	location = 'London'
+	YOUR_API_KEY = 'AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I'
 
-google_places = GooglePlaces('AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I')
-MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/'
-		 'json?query=tourist+attraction+'+location+
-		 '&key=AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I')  #grabbing the JSON result
-response = urllib.urlopen(MyUrl)
-jsonRaw = response.read()
-jsonData = json.loads(jsonRaw)
-print(type(jsonData))
+	google_places = GooglePlaces('AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I')
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/'
+			 'json?query=tourist+attraction+'+location+
+			 '&key=AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I')  #grabbing the JSON result
+	response = urllib.urlopen(MyUrl)
+	jsonRaw = response.read()
+	jsonData = json.loads(jsonRaw)
+	print(type(jsonData))
 
-j = json.dumps(jsonData,sort_keys=True, indent=4)
+	j = json.dumps(jsonData,sort_keys=True, indent=4)
 
-with open('Tests.json','w') as outf:
-	outf.write(j)
+	#with open('Tests.json','w') as outf:
+	#	outf.write(j)
+	return j
+
+if __name__ == "__main__":
+    app.run()
---
+++ test.py
@@ -0,0 +1,35 @@
+#import googleplaces
+
+import json,urllib
+
+from googleplaces import GooglePlaces, types, lang
+
+YOUR_API_KEY = 'AIzaSyBV_VtVaZJfy54pQiviMq2ZFllqQokZE3I'
+
+google_places = GooglePlaces(YOUR_API_KEY)
+
+query_result = google_places.nearby_search(
+        location='New+York, America',keyword='attraction')
+
+res = query_result.raw_response
+
+dic = []
+for place in query_result.places:
+    # Returned places from a query are place summaries.
+    place1={}
+    place.get_details()
+    place1['name'] = place.name
+    place1['website'] = place.website
+    place1['place_id'] = place.place_id
+    #loc = place.geo_location
+    #lat = loc['lat']
+    #lng = loc['lng']
+    #print lat
+    dic.append(place1)
+
+j = json.dumps(dic,sort_keys=True, indent=4)
+
+with open('Tests.json','w') as outf:
+	outf.write(j)
+
+#print(dic)
