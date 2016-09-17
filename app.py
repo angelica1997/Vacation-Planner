50e6709906a423a0f55114a25a4e893200e199ff
Tiffany Chen <tiffanyc@noosh.com>
2016-09-17 08:54:24 -0400

    python to front end connecting

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
-
- 
-@app.route('/signUp')
-def signUp():
-    return render_template('main.html')
-
-@app.route('/signUpUser', methods=['POST'])
-def signUpUser():
-    user =  request.form['username'];
-    password = request.form['password'];
-    return json.dumps({'status':'OK','user':user,'pass':password});
  
 if __name__ == "__main__":
     app.run(debug=True)
