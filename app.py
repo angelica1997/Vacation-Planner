3369a49d950dd56cfa5ff17fa70c88bea9944ff3
Tiffany Chen <tiffanyc@noosh.com>
2016-09-17 14:15:42 -0400

    questionnaire

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import render_template, request, jsonify
 
-from googleplaces import GooglePlaces, types, lang
+from flask import render_template, request, jsonify, url_for
+
+#from googleplaces import GooglePlaces, types, lang
+
 import json
 import urllib
 
@@ -16,44 +19,46 @@ def get_location():
 def index():
     return render_template("index.html")
 
+
+
 @app.route("/_send_location")
 def send_location():
-
-	YOUR_API_KEY = 'AIzaSyBdND_8stEjVtLh_jZVxODQSwQpzNnFdMU'
-
-	google_places = GooglePlaces(YOUR_API_KEY)
-
-	LOCATION = get_location()
-	QUERY = 'attraction'
-	MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
-		'?query=tourist+attraction+%s'
-		'&key=%s') % (LOCATION, YOUR_API_KEY)
-           #grabbing the JSON result
-	response = urllib.urlopen(MyUrl)
-	jsonRaw = response.read()
-	jsonData = json.loads(jsonRaw)
-	print jsonData
-	results = jsonData['results']
-	dic = []
-	i = 0
-	for each in results:
-		name = each['name']
-		place_id = each['place_id']
-		icon = each['icon']
-		geo = each['geometry']
-		lat = geo['location']['lat']
-		lng = geo['location']['lng']
-		rank = i
-		dic.append((lat,lng,rank,place_id,icon,name))
-		i += 1
-	j = json.dumps(dic,sort_keys=True, indent=4)
+    YOUR_API_KEY = 'AIzaSyBdND_8stEjVtLh_jZVxODQSwQpzNnFdMU'
+    #google_places = GooglePlaces(YOUR_API_KEY)
+    LOCATION = get_location()
+    QUERY = 'attraction'
+    MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
+                 '?query=tourist+attraction+%s'+'&key=%s') % (LOCATION, YOUR_API_KEY)
+    #grabbing the JSON result
+    response = urllib.urlopen(MyUrl)
+    jsonRaw = response.read()
+    jsonData = json.loads(jsonRaw)
+    print jsonData
+    results = jsonData['results']
+    dic = []
+    i = 0
+    
+    for each in results:
+        name = each['name']
+        place_id = each['place_id']
+        icon = each['icon']
+        geo = each['geometry']
+        lat = geo['location']['lat']
+        lng = geo['location']['lng']
+        rank = i
+        dic.append((lat,lng,rank,place_id,icon,name))
+        i += 1
+    j = json.dumps(dic,sort_keys=True, indent=4)
 	#with open('Tests.json','w') as outf:
 	#	outf.write(j)
-    print j
-	return j
+    return j
 
 
 
 
+@app.route("/questionnaire")
+def questionnaire():
+    return render_template("questionnaire.html")
+ 
 if __name__ == "__main__":
     app.run(debug=True)
--- templates/index.js
+++ templates/index.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     $(".submit").click(function () {
         var location = $('destination').val();
         $(".home").hide(1000);
         $("body").css('background', '#cccccc');
+        $(".select-style").css('display', 'visible');
     });
-});
+});*/
